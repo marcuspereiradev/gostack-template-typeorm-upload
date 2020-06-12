e49e270812272c501c2abca2e874258c281652b9
Marcus Pereira <marcus.work@hotmail.com>
2020-06-12 19:25:58 -0300

    Finding a category, creating a category if it doesnt exist

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,19 +1,43 @@
 // import AppError from '../errors/AppError';
 
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
 import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+import CreateCategoryService from './CreateCategoryService';
 
 interface Request {
   title: string;
   type: 'income' | 'outcome';
   value: number;
+  category: string;
 }
 class CreateTransactionService {
-  public async execute({ title, type, value }: Request): Promise<Transaction> {
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
     const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
 
-    const transaction = transactionRepository.create({ title, type, value });
+    let transactionCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!transactionCategory) {
+      const createCategory = new CreateCategoryService();
+
+      transactionCategory = await createCategory.execute({ title: category });
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+      category: transactionCategory,
+    });
 
     await transactionRepository.save(transaction);
 
