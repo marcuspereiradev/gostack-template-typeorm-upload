028ee4c4088769b1e92a327b40962e0e46856460
Marcus Pereira <marcus.work@hotmail.com>
2020-06-17 15:26:08 -0300

    Persisting category in database

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,6 +1,9 @@
 import csvParse from 'csv-parse';
 import fs from 'fs';
+import { getCustomRepository, getRepository, In } from 'typeorm';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
 
 interface CSVTransaction {
   title: string;
@@ -10,6 +13,9 @@ interface CSVTransaction {
 }
 class ImportTransactionsService {
   async execute(filePath: string): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
     const contactsReadStream = fs.createReadStream(filePath);
 
     const parsers = csvParse({
@@ -34,6 +40,26 @@ class ImportTransactionsService {
     });
 
     await new Promise(resolve => parseCSV.on('end', resolve));
+
+    const existentCategories = await categoryRepository.find({
+      where: { title: In(categories) },
+    });
+
+    const existentCategoriesTitles = existentCategories.map(
+      (category: Category) => category.title,
+    );
+
+    const addCategoryTitles = categories
+      .filter(category => !existentCategoriesTitles.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoryRepository.create(
+      addCategoryTitles.map(title => ({
+        title,
+      })),
+    );
+
+    await categoryRepository.save(newCategories);
   }
 }
 
